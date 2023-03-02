14063e0fab3337e8c9f73f36c87a2ff617388b5c
brovnie-ubuntu <maarl.deenis@gmail.com>
2023-03-02 22:19:12 +0100

    Changed functions name: from getInput to handleSubmit

--- client/src/features/Search/Search.jsx
+++ client/src/features/Search/Search.jsx
@@ -3,8 +3,7 @@ import { FormField } from '../../components'
 
 const Search = (props) => {
     const searchInput = useRef("");
-
-    const getInput = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         console.log(searchInput.current.value);
         props.isLoading(true);
@@ -13,7 +12,7 @@ const Search = (props) => {
     }
 
   return (
-    <form onSubmit={getInput}>   
+    <form onSubmit={handleSubmit}>   
         <div className="relative grid grid-cols-12 gap-5">
             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
