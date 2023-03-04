6bcf1c3ade6737027e1813c7a0ad9652c35e4070
brovnie-ubuntu <maarl.deenis@gmail.com>
2023-03-04 22:17:24 +0100

    :sparkles: (generateImage) added loader

--- client/src/features/GenerateImage/GenerateImage.jsx
+++ client/src/features/GenerateImage/GenerateImage.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Image from "./Image";
 import ImageForm from './ImageForm';
 
 const GenerateImage = () => {
+  const [loading, setLoading] = useState(false);
   return (
     <div className='mt-5 mb-5'>
     <p>Step 1. Find image</p>
-        <ImageForm/>
-        <Image isLoading={false}/>
+        <ImageForm isLoading={setLoading}/>
+        <Image isLoading={loading}/>
     </div>
   )
 }
