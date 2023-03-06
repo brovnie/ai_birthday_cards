3b5cc91941d58eaf9c444ea5597d2c280ed15601
brovnie-ubuntu <maarl.deenis@gmail.com>
2023-03-06 14:36:41 +0100

    :construction: (request api openAI) prepare post api request for frontend

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -17,4 +17,23 @@ router.route('/').get( (req,res) => {
     res.send("Hello from openai")
 })
 
+router.route('/').post(async (req,res) => {
+    try {
+        const { prompt } = req.body;
+        const apiResponse = await openai.createImage({
+            prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json'
+        })
+
+        const image = apiResponse.data.data[0].b64_json;
+
+        res.status(200).json({ photo: image });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error?.response.data.error.message);
+    }
+})
+
 export default router;
