948b757624665f5b601e4e159a8e104eb25e6658
brovnie-ubuntu <maarl.deenis@gmail.com>
2023-03-08 13:03:59 +0100

    :card_file_box: (schema) updated BirthdayCard schema

--- server/mongodb/models/birthdaycard.js
+++ server/mongodb/models/birthdaycard.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const BirthdayCard = new mongoose.Schema({
     name: { type: String, required: true },
+    celebraitsName: { type: String, required: true },
     prompt: { type: String, required: true },
     wishes: { type: String, required: false },
     foto: { type: String, required: true },
